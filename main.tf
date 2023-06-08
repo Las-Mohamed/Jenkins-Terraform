efd7d8eb674a223ed87c5d8ada0cf391ccdbbee9
Las-Mohamed <128605474+Las-Mohamed@users.noreply.github.com>
2023-06-08 14:45:25 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@ terraform {
 }
 
 provider "azurerm" {
-   subscription_id = var.subscription_id
-   client_id = var.client_id
-   client_secret = var.client_secret
-   tenant_id = var.tenant_id
+   subscription_id = var.subscriptionid
+   client_id = var.clientid
+   client_secret = var.clientsecret
+   tenant_id = var.tenantid
    skip_provider_registration = true
    features {}
 }
