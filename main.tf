aed07666372ecb8f66c9dfb84032da37e789ec91
Las-Mohamed <128605474+Las-Mohamed@users.noreply.github.com>
2023-06-08 14:34:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,18 @@
- terraform {
-
-   required_version = ">=0.12"
-
+terraform {
+   required_version = ">= 0.12"
    required_providers {
-     azurerm = ">2.4"
+      azurerm = ">3.0"
    }
- }
+}
 
- provider "azurerm" {
+provider "azurerm" {
+   subscription_id = var.subscription_id
+   client_id = var.client_id
+   client_secret = var.client_secret
+   tenant_id = var.tenant_id
+   skip_provider_registration = true
    features {}
- }
+}
 
 
  resource "azurerm_resource_group" "webserver" {
