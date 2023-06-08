02de5235dd222cdaf302e1d1254bda941e1e3729
Las-Mohamed <128605474+Las-Mohamed@users.noreply.github.com>
2023-06-08 14:11:31 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,10 +3,7 @@
    required_version = ">=0.12"
 
    required_providers {
-     azurerm = {
-       source = "hashicorp/azurerm"
-       version = "~>2.0"
-     }
+     azurerm = ">3.0"
    }
  }
 
