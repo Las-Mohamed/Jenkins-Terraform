8b92fb3b8c54a8fbfae10a3c43c03d74f3e88e36
Las-Mohamed <128605474+Las-Mohamed@users.noreply.github.com>
2023-06-09 11:21:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,10 @@
 terraform {
   required_version = ">=0.12"
-
    required_providers {
-   #  azurerm = {
-   #    source = "hashicorp/azurerm"
-   #    version = "~>2.0"
-   #  }
-   #}
-   #required_version = ">= 0.12"
-   #required_providers {
       azurerm = ">3.0"
    }
 }
-
 provider "azurerm" {
-  # subscription_id = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
-  # client_id = $AZURE_CLIENT_ID
-  # client_secret = $AZURE_CLIENT_SECRET
-  # tenant_id = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
-  # skip_provider_registration = true
    features {}
 }
 
